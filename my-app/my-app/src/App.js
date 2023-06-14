89db828bffcb98ff2ab798acb6eef281b0d07d95
Islom_dev <inogomovislom@gmail.com>
2023-06-14 16:25:41 +0500

    npx orqali yaratildi

---
+++ my-app/my-app/src/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Salom Dasturchi</h1>
+      <header className="App-header">
+        <img src={"./image/logo.svg"} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
